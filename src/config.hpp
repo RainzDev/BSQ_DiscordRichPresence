b029244b073c6a801c8a6e1c8052f61bc718aaca
RainzDev <michael08pokrovsski16@gmail.com>
2026-04-30 00:00:35 +0400

    update default config value

--- src/config.hpp
+++ src/config.hpp
@@ -6,6 +6,6 @@
 
 DECLARE_CONFIG(Config) {
     CONFIG_VALUE(PCIPSetting, std::string, "Private IP", "");
-    CONFIG_VALUE(PortSetting, std::string, "Port", "");
+    CONFIG_VALUE(PortSetting, std::string, "Port", "8080");
     CONFIG_VALUE(FirstTime, bool, "First Time", true);
 };
